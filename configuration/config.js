b4c1c339f03448eead0f8774f147fd9dfb971e35
dev <51611804+dev363@users.noreply.github.com>
2019-09-19 22:24:20 +0530

    update config.js

--- configuration/config.js
+++ configuration/config.js
@@ -4,8 +4,8 @@ module.exports = {
    * It consists of Twitter app information, database information.
    */
 
-  facebook_api_key: "2412194822345505",
-  facebook_api_secret: "e98e5ff1702592493b6951f6bc4c6c89",
+  facebook_api_key: "******************",
+  facebook_api_secret: "******************",
   callback_url: "http://localhost:8070/auth/facebook/callback",
   use_database: true,
   host: "localhost",
